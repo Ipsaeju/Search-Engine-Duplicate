6c4d102ae4461ddc885bfa6f8b23257833722ba2
Ipsaeju <36467114+Ipsaeju@users.noreply.github.com>
2018-09-25 22:07:02 -0700

    added changes to token processor

---
+++ 429Project1/src/cecs429/text/NonAlphaProcessor.java
@@ -0,0 +1,67 @@
+package cecs429.text;
+
+import cecs429.text.TokenProcessor;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Jonalyn Razon
+ */
+public class NonAlphaProcessor implements TokenProcessor{
+    private String first = "";
+    private String last = "";
+    private String processedToken = "";
+    private ArrayList<String> tokensList = new ArrayList<String>();
+    private int prevHyphenIndex = 0;
+    private int currHyphenIndex = 0;
+
+    @Override
+    public ArrayList<String> processToken(String token) {
+            int tokenLen = token.length();
+            if(tokenLen == 1){
+                first = token.substring(0, tokenLen);
+                processedToken = token;
+                if (!first.matches("^.*[^a-zA-Z0-9].*$")){
+                processedToken = token.replace(first, "");
+                }
+                tokensList.add(processedToken);
+                return tokensList;
+            }
+            else{
+                first = token.substring(0, 1);
+                last = token.substring(token.length(), token.length());
+                processedToken = token;
+                if (!first.matches("^.*[^a-zA-Z0-9].*$")){
+                    processedToken = processedToken.replace(first, "");
+                }
+                if (!last.matches("^.*[^a-zA-Z0-9].*$")){
+                    processedToken = processedToken.replace(last, "");
+                }
+                if (processedToken.contains("'")){
+                    processedToken = processedToken.replaceAll("'", "");
+                }
+                if (processedToken.contains("\"")){
+                    processedToken = processedToken.replaceAll("\"", "");
+                }
+                if (processedToken.contains("`")){
+                    processedToken = processedToken.replaceAll("`", "");
+                }
+                if (processedToken.contains("-")){
+                    for(int i = 0; i < processedToken.length(); i++){
+                        if(processedToken.charAt(i) == '-'){
+                            prevHyphenIndex = currHyphenIndex;
+                            currHyphenIndex = i;
+                            String hyphenSplit = processedToken.substring(prevHyphenIndex, currHyphenIndex);
+                            hyphenSplit = hyphenSplit.replaceAll("-", "");
+                            tokensList.add(hyphenSplit);
+                        }
+                    }
+                    processedToken = processedToken.replaceAll("-", "");
+                }
+                tokensList.add(processedToken);
+                return tokensList;
+            }
+    }
+
+}
